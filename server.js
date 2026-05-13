6accb09ae591539424db402c88d52a3b4123d44a
Mebantei <mebanteisalahe@gmai.com>
2026-05-13 16:51:06 +0530

    Fixed Mobile Subscriber Issue

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const fs = require("fs");
 const { SerialPort } = require("serialport");
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 // ================= ARDUINO SERIAL =================
